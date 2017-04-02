1ed1d85acc58f94d0a4f06271ed91f15b9c41edc
Air_lml <airming@users.noreply.github.com>
2017-04-02 11:41:31 +0800

    网络事件
    增加底层poll与epoll封装

--- jmuduo/muduo/net/EventLoop.cc
+++ jmuduo/muduo/net/EventLoop.cc
@@ -9,8 +9,10 @@
 #include <muduo/net/EventLoop.h>
 
 #include <muduo/base/Logging.h>
+#include <muduo/net/Channel.h>
+#include <muduo/net/Poller.h>
 
-#include <poll.h>
+//#include <poll.h>
 
 using namespace muduo;
 using namespace muduo::net;
@@ -20,6 +22,7 @@ namespace
 	// 当前线程EventLoop对象指针
 	// 线程局部存储
 	__thread EventLoop* t_loopInThisThread = NULL;
+	const int kPollTimeMs = 10000;
 }
 
 EventLoop* EventLoop::getEventLoopOfCurrentThread()
@@ -29,7 +32,11 @@ EventLoop* EventLoop::getEventLoopOfCurrentThread()
 
 EventLoop::EventLoop()
 	:looping_(false),
-	threadId_(CurrentThread::tid())
+	 quit_(false),
+	 eventHandling_(false),	 
+	 threadId_(CurrentThread::tid()),
+	 poller_(Poller::newDefaultPoller(this)),
+	 currentActiveChannel_(NULL)
 {
 	LOG_TRACE << "EventLoop created " << this << " in thread " << threadId_;
   // 如果当前线程已经创建了EventLoop对象，终止(LOG_FATAL)
@@ -54,15 +61,64 @@ EventLoop::~EventLoop()
 void EventLoop::loop()
 {
 	assert(!looping_);
-	assertInLoopThread();
-	looping_ = true;
+  	// 断言当前处于创建该对象的线程中
+  	assertInLoopThread();
+  	looping_ = true;
+  	LOG_TRACE << "EventLoop " << this << " start looping";
 
-	LOG_TRACE << "EventLoop " << this << " start looping";
-
-	::poll(NULL, 0, 5*1000);
+  	//::poll(NULL, 0, 5*1000);
+	while (!quit_)
+	{
+	    activeChannels_.clear();
+	    pollReturnTime_ = poller_->poll(kPollTimeMs, &activeChannels_);
+	    //++iteration_;
+	    if (Logger::logLevel() <= Logger::TRACE)
+	    {
+	      printActiveChannels();
+	    }
+	    // TODO sort channel by priority
+	    eventHandling_ = true;
+	    for (ChannelList::iterator it = activeChannels_.begin();
+	        it != activeChannels_.end(); ++it)
+	    {
+	      currentActiveChannel_ = *it;
+	      currentActiveChannel_->handleEvent(pollReturnTime_);
+	    }
+	    currentActiveChannel_ = NULL;
+	    eventHandling_ = false;
+	    //doPendingFunctors();
+	}
 
 	LOG_TRACE << "EventLoop " << this << " stop looping";
 	looping_ = false;
+}
+
+void EventLoop::quit()
+{
+  quit_ = true;
+  if (!isInLoopThread())
+  {
+    //wakeup();
+  }
+}
+
+void EventLoop::updateChannel(Channel* channel)
+{
+    assert(channel->ownerLoop() == this);
+    assertInLoopThread();
+    poller_->updateChannel(channel);
+}
+
+void EventLoop::removeChannel(Channel* channel)
+{
+    assert(channel->ownerLoop() == this);
+    assertInLoopThread();
+    if (eventHandling_)
+    {
+        assert(currentActiveChannel_ == channel ||
+        std::find(activeChannels_.begin(), activeChannels_.end(), channel) == activeChannels_.end());
+    }
+    poller_->removeChannel(channel);
 }
 
 void EventLoop::abortNotInLoopThread()
@@ -70,4 +126,14 @@ void EventLoop::abortNotInLoopThread()
   LOG_FATAL << "EventLoop::abortNotInLoopThread - EventLoop " << this
             << " was created in threadId_ = " << threadId_
             << ", current thread id = " <<  CurrentThread::tid();
+}
+
+void EventLoop::printActiveChannels() const
+{
+  	for (ChannelList::const_iterator it = activeChannels_.begin();
+      it != activeChannels_.end(); ++it)
+	{
+	    const Channel* ch = *it;
+	    LOG_TRACE << "{" << ch->reventsToString() << "} ";
+	}
 }
--- jmuduo/muduo/net/EventLoop.h
+++ jmuduo/muduo/net/EventLoop.h
@@ -11,15 +11,21 @@
 #ifndef MUDUO_NET_EVENTLOOP_H
 #define MUDUO_NET_EVENTLOOP_H
 
+#include <vector>
+
 #include <boost/noncopyable.hpp>
+#include <boost/scoped_ptr.hpp>
 
 #include <muduo/base/CurrentThread.h>
 #include <muduo/base/Thread.h>
+#include <muduo/base/Timestamp.h>
 
 namespace muduo
 {
 	namespace net
 	{
+		class Channel;
+		class Poller;
 		///
 		/// Reactor, at most one per thread.
 		///
@@ -30,12 +36,23 @@ namespace muduo
 			EventLoop();
 			~EventLoop();	// force out-line dtor, for scoped_ptr members.
 
-		  ///
-		  /// Loops forever.
-		  ///
-		  /// Must be called in the same thread as creation of the object.
-		  ///
+			///
+			/// Loops forever.
+			///
+			/// Must be called in the same thread as creation of the object.
+			///
 		  	void loop();
+
+		  	void quit();
+
+			///
+			/// Time when poll returns, usually means data arrivial.
+			///
+			Timestamp pollReturnTime() const { return pollReturnTime_; }
+
+			// internal usage
+			void updateChannel(Channel* channel);		// 在Poller中添加或者更新通道
+			void removeChannel(Channel* channel);		// 从Poller中移除通道
 
 		  	void assertInLoopThread(){
 		  		if (!isInLoopThread())
@@ -52,8 +69,18 @@ namespace muduo
 
 		 private:
 		 	void abortNotInLoopThread();
+
+		 	void printActiveChannels() const; // DEBUG
+
+  			typedef std::vector<Channel*> ChannelList;
 		 	bool looping_; /* atomic */
-		 	const pid_t threadId_; //Current class threadid			
+			bool quit_; /* atomic */
+			bool eventHandling_; /* atomic */
+			const pid_t threadId_;		// 当前对象所属线程ID
+			Timestamp pollReturnTime_;
+			boost::scoped_ptr<Poller> poller_;
+			ChannelList activeChannels_;		// Poller返回的活动通道
+			Channel* currentActiveChannel_;	// 当前正在处理的活动通道	
 		};
 	}
 }
